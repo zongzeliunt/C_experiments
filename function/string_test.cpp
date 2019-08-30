8c00cfe30f3060b2ef6acaf5d5ce30cf8387aa85
zongzeliunt <zongzeli2@my.unt.edu>
2019-08-30 14:46:45 -0600

    add c++ test case

---
+++ function/string_test.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string a = "abc";
+	cout<<a<<endl;
+	
+
+
+}
---
+++ function/unsign_test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	unsigned a = 10;
+	int b = 42;
+
+	cout<<"unsigned sub int: "<< a - b <<endl;
+	
+	/*
+	dead looping
+	for (unsigned i = 10; i >= 0; --i) {
+		cout<<i<<endl;
+	}
+	*/
+
+
+}
