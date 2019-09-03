60445dbbd9d1fb17c977d3e5b8cdada3abed1bcf
zongzeliunt <zongzeli2@my.unt.edu>
2019-09-03 13:30:54 -0600

    change datatype_test.cpp

--- function/datatype_test.cpp
+++ function/datatype_test.cpp
@@ -1,12 +1,25 @@
 #include <string>
 #include <iostream>
+#include <typeinfo>
 
 using namespace std;
 
 int main() {
-	string a = "abc";
-	cout<<a<<endl;
+	string a = "abcde";
+	cout<<"string a "<<a<<" "<<typeid(a).name()<<" "<<sizeof(a)<<endl;	
+
+	int b = 10;
+	cout<<"int b "<<b<<" "<<typeid(b).name()<<" "<<sizeof(b)<<endl;	
 	
+	float c = 50;
+	cout<<"float c "<<c<<" "<<typeid(c).name()<<" "<<sizeof(c)<<endl;	
+	
+	char d = 'd';
+	cout<<"char d "<<d<<" "<<typeid(d).name()<<" "<<sizeof(d)<<endl;
 
+	string e = a + "gh";
+	cout<<"string e "<<e<<" "<<typeid(e).name()<<" "<<sizeof(e)<<endl;
 
+	cout<<e[6]<<endl;
+	
 }
