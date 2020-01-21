aaed4286a9fca6f8a465a7caa03d8071e11776f0
zongzeliunt <zongzeli2@my.unt.edu>
2020-01-21 17:41:07 -0600

    add gdb case

---
+++ GDB/hello_world.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int add (int a, int b){
+	return a + b;
+}
+
+
+
+int main () {
+	int a, b, c;
+	a = 1;
+	b = 2;
+	c = add(a, b);
+
+	int d = 100;
+	cout<<"c value: "<<c<<endl;	
+	cout<<"hello world"<<endl;
+
+
+}
