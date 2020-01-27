95f40b73df784b241cd96ccfe3f1fc497b6f4a3f
zongzeliunt <zongzeli2@my.unt.edu>
2020-01-26 19:38:08 -0600

    merge switch

--- exam_case/switch_test.cpp
+++ exam_case/switch_test.cpp
@@ -1,3 +1,30 @@
+//每一个switch-case必须加上break，否则能match一个就顺序把下面所有的case全执行一遍，如果把default放最上面又不写break，则下面的条件全执行！
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+main( )
+{
+   int i, n=0;
+   for(i=0; i<5; i++)
+   {
+		cout<<"i value: "<<i<<endl;
+      switch(i)
+      {
+         default:
+            cout<<"default"<<endl;
+         case 1:
+            cout<<"case 1 i value: "<<i<<endl;
+         case 3:
+            cout<<"case 3 i value: "<<i<<endl;
+         case 4:
+            cout<<"case 4 i value: "<<i<<endl;
+      }
+   }
+}
+
+
+/*
 //Qe1Qt2
 
 #include <stdio.h>
@@ -27,3 +54,4 @@ main( )
       }
    }
 }
+*/
