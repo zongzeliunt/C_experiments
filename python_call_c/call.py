79a40a257016f3d8dbce40ad09bbcfbdeac3029d
ZongzeLi <zongze.li@tetramem.com>
2022-02-15 14:35:31 -0800

    update python call c

--- python_call_c/call.py
+++ python_call_c/call.py
@@ -1,4 +1,10 @@
+#https://docs.python.org/3/library/ctypes.html
+#c_uint
+#c_char_p
+
 from ctypes import *
+
+
 so_file = "./functions.so"
 my_functions = CDLL(so_file)
 
@@ -8,4 +14,10 @@ print(my_functions.get_tmp_value())
 my_functions.hello()
 
 print(my_functions.cal(50))
+name = "" * 64
+name = "ares"
 
+s=c_char_p()
+s.value = b"ares"
+
+my_functions.show_string(s)
--- python_call_c/flow.c
+++ python_call_c/flow.c
@@ -16,5 +16,14 @@ uint32 cal (uint32 input)
     return input + tmp_value;
 }
 
+void show_string(char name[64])
+{
+    char name_1[64];
+    strcpy(name_1, "ares_1"); 
+    
+    char name_2[64];
+    strcpy(name_2, name); 
 
+    printf ("show string: %s, %s\n", name, name_1);
+}
 
